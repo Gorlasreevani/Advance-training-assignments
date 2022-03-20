08001c8ea8719295dc5efcace523f3ec60054fa4
Gorlasreevani <97828270+Gorlasreevani@users.noreply.github.com>
2022-03-20 17:02:12 +0530

    Add files via upload

---
+++ Problem Statement-7.2/src/com/users/AgeNotWithInRangeException.java
@@ -0,0 +1,11 @@
+package com.users;
+
+
+	class AgeNotWithInRangeException extends Exception
+	{
+	     public String toString()
+	     {
+	          return ("Age is not between 15 and 21. please ReEnter the Age");
+	     }
+	}
+
